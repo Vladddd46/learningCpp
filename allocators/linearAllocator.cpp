94c23efb0b654588395a41f2b89ab5273fc6f471
vdepeshko <vdepeshko@VDEPESHKO-NOUT>
2021-02-21 06:12:15 -0800

    'linearAllocator_improved'

--- allocators/linearAllocator.cpp
+++ allocators/linearAllocator.cpp
@@ -6,8 +6,11 @@
 
 using namespace std;
 
+static int memSize = 100;
+
+// free mem range and merge them
 template <class T>
-class MyAlloc {
+class linearAllocator {
 public:
     typedef T              value_type;
     typedef T*             pointer;
@@ -18,54 +21,53 @@ public:
     typedef std::ptrdiff_t difference_type;
 
     void print() {
-        cout << "startAddress=" << startAddress << " offset=" << offset << " endAddress=" << endAddress << endl << endl;
+        printf("\tstartAddress=%d endAddress=%d. All allocated memory=%dbytes.\n", startAddress, endAddress, endAddress-startAddress);
     }
 
-    MyAlloc(std::size_t count = 5) : offset(nullptr), startAddress(nullptr) {
-        cout << "[D] constructor method called" << endl;
-        startAddress = (pointer)(operator new(count * sizeof(T)));
-        offset       = startAddress;
-        endAddress   = startAddress + count;
+
+    /* Allocates block of memory (100 bytes by default).
+     * Initialize pointers,
+     * startAddress - begin of allocated memory.
+     * endAddress   - end of allocated memory.
+     * 
+     */
+    linearAllocator(std::size_t count = memSize) {
+        startAddress = (uint8_t *)(operator new(count * sizeof(T)));
+        endAddress  = startAddress + (count * sizeof(T));
+        pushBack(&allocedBlocksList, endAddress, nullptr);
+        cout << "[D] constructor method called:" << endl;
+        cout << "\t" << "Block of memmory was allocated: " << count * sizeof(T) << endl;
+        printList();
         print();
+        cout << endl << endl;
     }
 
-    ~MyAlloc() {
-        cout << "[D] destructor method called" << endl;
+    ~linearAllocator() {
         clear();
-        print();
     }
 
     template <class U>
-    MyAlloc(const MyAlloc<U>&) noexcept {}
-
-    void memRealloc() {
-        cout << "[D] Reallocate method called" << endl;
-        int num = (endAddress - startAddress) * 2;
-        // delete[] startAddress;    
-        startAddress = (pointer)(operator new(num * sizeof(T)));
-        offset       = startAddress;
-        endAddress   = startAddress + num;
-    }
+    linearAllocator(const linearAllocator<U>&) noexcept {}
 
     pointer allocate(size_type count) {
-        cout << "[D] allocate method called" << endl;
-       
-        if (offset != nullptr && offset + count > endAddress) {
-            memRealloc(); // !!!!!!!!!!!!!
+        cout << "[D] Allocate method called" << endl;
+        size_type numBytesToAlloc = count * sizeof(T);
+        cout << "\tBytes to allocate=" << count * sizeof(T) << " =>" << count << " object(s) of " << sizeof(T) << " size" << endl;
+        uint8_t *memBlock = getMemoryBlock(numBytesToAlloc);
+        if (memBlock == nullptr) {
+            clear();
+            pushBack(&allocedBlocksList, endAddress, nullptr);
+            memBlock = getMemoryBlock(numBytesToAlloc);
         }
-        pointer res = offset;
-        offset  +=  count;
-        print();
-        return res;
+        printList();
+        return (pointer)memBlock;
     }
 
     void deallocate(pointer p, size_type count) noexcept {
-        cout << "[D] deallocate method called" << endl;
-
-        if (offset != nullptr && offset > endAddress) {
-            clear();
-        }
-        print();
+        cout << "[D] Deallocate method called" << endl;
+        printf("\tRemoving mem block with start addr: %d\n", p);
+        removeNodeByStartAddr(&allocedBlocksList, (uint8_t *)p);
+        printList();
     }
 
     void destroy(T *p) {
@@ -74,39 +76,187 @@ public:
 
     void clear() {
         cout << "[D] clear method called" << endl;
+        printf("\tFreeing all memory from startAddress: %d\n", startAddress);
         if (startAddress != nullptr){
             delete[] startAddress;
             startAddress = nullptr;
         }
-        offset = nullptr;
-        print();
+        endAddress = nullptr;
+        freeList(&allocedBlocksList);
     }
+
+    void listTest() {
+        list_t *testList = nullptr;
+        uint8_t a  = 0;
+        uint8_t a1 = 1;
+        uint8_t a2 = 2;
+        uint8_t a3 = 3;
+        uint8_t a4 = 4;
+        uint8_t a5 = 5;
+        insertInOrder(&testList, &a,  &a);
+        insertInOrder(&testList, &a1, &a);
+        insertInOrder(&testList, &a3, &a);
+        insertInOrder(&testList, &a4, &a);
+        insertInOrder(&testList, &a5, &a);
+        insertInOrder(&testList, &a2, &a);
+        while(testList) {
+            printf("startAddr = %d\n", testList->startAddressOfBlock);
+            testList = testList->next;
+        }
+    }
+
 private:
-    pointer    startAddress = nullptr;
-    pointer    endAddress   = nullptr;
-    pointer    offset       = nullptr;
+    typedef struct list_s {
+        uint8_t *startAddressOfBlock;
+        uint8_t *endAddressOfBlock;
+        struct list_s *next; 
+    } list_t;
+
+    uint8_t *startAddress      = nullptr;
+    uint8_t *endAddress        = nullptr;
+    list_t  *allocedBlocksList = nullptr;
+
+    list_t *createNode(uint8_t *startAddr, uint8_t *endAddr) {
+        list_t *node = new list_t;
+        node->startAddressOfBlock = startAddr;
+        node->endAddressOfBlock   = endAddr;
+        node->next = nullptr;
+        return node;
+    }
+
+    uint8_t *getMemoryBlock(size_type memSize) {
+        cout << "\t[D] getMemoryBlock function called" << endl;
+        uint8_t *prevAddr = startAddress;
+        list_t *tmp = allocedBlocksList;
+
+        while(tmp != nullptr) {
+            if (tmp->startAddressOfBlock - prevAddr > memSize) {
+                printf("\t\tMem with startAddr=%d returned. endAddr=%d. Block of %d bytes alloced\n", prevAddr, prevAddr + memSize, memSize);
+                insertInOrder(&allocedBlocksList, prevAddr, prevAddr + memSize);
+                return prevAddr;
+            }
+            prevAddr = tmp->endAddressOfBlock;
+            tmp = tmp->next;
+        }
+        printf("\tMem can not be allocated. NUll is returned\n\n");
+        return nullptr; // never happens
+    }
+
+    // @brief Removes node with startAddrToDelete from list.
+    void removeNodeByStartAddr(list_t **list, uint8_t *startAddrToDelete) {
+        if (*list == nullptr) {
+            return;
+        }
+        list_t *tmp = *list;
+        list_t *nodeToFree = nullptr;
+        if (tmp->startAddressOfBlock == startAddrToDelete) {
+            nodeToFree = *list;
+            *list = tmp->next;
+            delete nodeToFree;
+        }
+        while(tmp) {
+            if (tmp->next != nullptr && tmp->next->startAddressOfBlock == startAddrToDelete) {
+                nodeToFree = tmp->next;
+                printf("\t[D] node with startAddr %d was deleted\n", startAddrToDelete);
+                tmp->next =tmp->next->next;
+                delete nodeToFree;
+                break;
+            }
+            tmp = tmp->next;
+        }
+    }
+
+    // @brief Inserts node in sorted order. (sort by list->startAddressOfBlock)
+    void insertInOrder(list_t **list, uint8_t *startAddr, uint8_t *endAddr) {
+        list_t *node = createNode(startAddr, endAddr);
+
+        list_t *tmp = *list;
+        if (*list == nullptr) {
+            *list = node;
+        }
+        else if (tmp->startAddressOfBlock > node->startAddressOfBlock) {
+            node->next = *list;
+            *list = node;
+        }
+        else  {
+            while(tmp) {
+                if (tmp && tmp->next->startAddressOfBlock > node->startAddressOfBlock) {
+                    node->next = tmp->next;
+                    tmp->next = node;
+                    break;
+                }
+                else if (tmp && tmp->next == nullptr) {
+                    tmp->next = node;
+                    break;
+                }
+                tmp = tmp->next;
+            }
+        }
+    }
+
+    void pushBack(list_t **list, uint8_t *startAddr, uint8_t *endAddr) {
+        list_t *node = createNode(startAddr, endAddr);
+        if (*list == nullptr) {
+            *list = node;
+        }
+        else {
+            list_t *tmp = *list;
+            while(tmp->next != nullptr) {
+                tmp = tmp->next;
+            }
+            tmp->next = node;
+        }
+    }
+
+    void freeList(list_t **list) {
+        list_t *nodeToDel = nullptr;
+        list_t *tmp = *list;
+        while(tmp) {
+            nodeToDel = tmp;
+            tmp = tmp->next;
+            if (nodeToDel != nullptr) {
+                delete nodeToDel;
+            }
+        }
+        allocedBlocksList = nullptr;
+    }
+
+    void printList() {
+        cout << "\t[D] List printing" << endl;
+        int index = 0;
+        list_t *tmp = allocedBlocksList;
+        while(tmp) {
+            printf("\t\tNode_index=%d startAddr=%d endAddress=%d\n", index, tmp->startAddressOfBlock, tmp->endAddressOfBlock);
+            index++;
+            tmp = tmp->next;
+        }
+    }
 };
 
 // return that all specializations of this allocator are interchangeable
 template <class T1, class T2>
-bool operator== (const MyAlloc<T1>&, const MyAlloc<T2>&) throw() {
+bool operator== (const linearAllocator<T1>&, const linearAllocator<T2>&) throw() {
     return true;
 }
 
 template <class T1, class T2>
-bool operator!= (const MyAlloc<T1>&, const MyAlloc<T2>&) throw() {
+bool operator!= (const linearAllocator<T1>&, const linearAllocator<T2>&) throw() {
     return false;
 }
 
 
 int main() {
-    // create a vector, using MyAlloc<> as allocator
-    vector<int, MyAlloc<int>> v;
+    // create a vector, using linearAllocator<> as allocator
+    vector<int, linearAllocator<int>> v;
     v.push_back(1);
     v.push_back(2);
     v.push_back(3);
     v.push_back(4);
     v.push_back(5);
+    v.push_back(6);
+    v.push_back(7);
+    v.push_back(8);
+    v.push_back(9);
 
     for (auto i: v) {
         cout << i << endl;
